9d5cc05d43d00e23b7993ab5ec240f320d994c7a
Boris Gorelik <boris@gorelik.net>
2021-01-04 11:52:18 +0200

    Version update

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
+"""
+Setup a pip package
+
+How to upload a package to pypi:
+ * Update the version number
+ * Build the package `python setup.py bdist_wheel`
+ * Deploy: `python3 -m twine upload --repository-url https://upload.pypi.org/legacy/  dist/*`
+
+"""
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
 setuptools.setup(
     name="sshalosh-borisgorelik",
-    version="0.0.1",
+    version="0.0.3",
     author="Boris Gorelik",
     author_email="boris@gorelik.net",
     description="convenience functions for serialization to s3",
